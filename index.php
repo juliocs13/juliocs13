6213b76cd15345a0a6af1e20891e90823697f79b
juliocs13 <juliocesarcerqueira18@gmail.com>
2024-09-10 10:33:45 -0300

    ´codigo inicial com h1 h2

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Versionando o código com GIT </h1>
+    <H2>Aula 01 10/09</h2>
+</html>

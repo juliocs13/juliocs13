5f989eac117592d15a10be1f949833f533df7993
juliocs13 <juliocesarcerqueira18@gmail.com>
2024-09-10 11:26:26 -0300

    adcionado o estilos CSS

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width, initial-scale=1.0">
     <title>Document</title>
+    <link href="estiloo.css" rel="stylesheet
 </head>
 <body>
     <h1>Versionando o código com GIT </h1>
     <H2>Aula 01 10/09</h2>
     <p>Nesta aula aprendemos a versionar de forma local</p>
-    
+
 </body>    
 </html>
